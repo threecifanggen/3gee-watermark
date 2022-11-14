4ef96af6d4bcb4a17017744cfcfc06df6a1eebe8
3gee <huangbaochenwo@live.com>
2022-11-14 20:58:35 +0800

    增加关于页面的说明

--- app/gui/about_dialog.py
+++ app/gui/about_dialog.py
@@ -3,8 +3,12 @@ from PySide6 import QtWidgets
 from ..core import VERSION
 
 class AboutDialog(QtWidgets.QDialog):
+    """「关于本软件」
+    """
     
     def __init__(self):
         super().__init__()
         self.setWindowTitle("关于本软件")
         self.setFixedSize(200, 200)
+        label = QtWidgets.QLabel(f"版本号：v{VERSION}。\n本软件由3GEE开发。", self)
+        label.setMargin(30)
